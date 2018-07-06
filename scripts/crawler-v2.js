53544a2652eb6a5d8c8a522f9d208f6f4b771a01
Yuanchao Sun <yuanchao.sun@gmail.com>
2018-07-05 21:15:28 -0700

    Update

--- scripts/crawler-v2.js
+++ scripts/crawler-v2.js
@@ -35,22 +35,21 @@ const puppeteer = require('puppeteer')
     for (let link of links) {
       try {
         await page.goto(link)
-        const nameSelector =
-          '#ContentPlaceHolder1_contractCodeDiv > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(2)'
-        await page.waitForSelector(nameSelector)
-        const contractName = await page.evaluate(nameSelector => {
-          return document
-            .querySelector(nameSelector)
-            .textContent.replace(/^\s+|\s+$/g, '')
-        }, nameSelector)
-        console.log(contractName)
+        const symbolSelector =
+          '#ContentPlaceHolder1_tr_tokeninfo > td:nth-child(2) > a'
+        await page.waitForSelector(symbolSelector, { timeout: 10000 })
+        const symbol = await page.evaluate(symbolSelector => {
+          const tokenInfo = document.querySelector(symbolSelector).textContent
+          return tokenInfo.match(/\(([^)]+)\)/)[1].replace(/^\s+|\s+$/g, '')
+        }, symbolSelector)
+        console.log(symbol)
         const sourceSelector = '#editor'
         await page.waitForSelector(sourceSelector)
         const sourceCode = await page.evaluate(
           'var editor = ace.edit("editor"); editor.getValue();'
         )
 
-        const solPath = path.resolve(__dirname, 'sol', contractName + '.sol')
+        const solPath = path.resolve(__dirname, 'sol', symbol + '.sol')
         await fs.writeFile(solPath, sourceCode, 'utf-8', err => {
           if (err) {
             console.error(err)
