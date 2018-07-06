e48e4683d54ed4848671685ee97c1f4ffe0f32ae
Yuanchao Sun <yuanchao.sun@gmail.com>
2018-07-05 21:46:41 -0700

    Update

--- scripts/crawler-v2.js
+++ scripts/crawler-v2.js
@@ -35,21 +35,22 @@ const puppeteer = require('puppeteer')
     for (let link of links) {
       try {
         await page.goto(link)
-        const symbolSelector =
+        const infoSelector =
           '#ContentPlaceHolder1_tr_tokeninfo > td:nth-child(2) > a'
-        await page.waitForSelector(symbolSelector, { timeout: 10000 })
-        const symbol = await page.evaluate(symbolSelector => {
-          const tokenInfo = document.querySelector(symbolSelector).textContent
-          return tokenInfo.match(/\(([^)]+)\)/)[1].replace(/^\s+|\s+$/g, '')
-        }, symbolSelector)
-        console.log(symbol)
+        await page.waitForSelector(infoSelector)
+        const tokenInfo = await page.evaluate(infoSelector => {
+          return document
+            .querySelector(infoSelector)
+            .textContent.replace(/^\s+|\s+$/g, '')
+        }, infoSelector)
+        console.log(tokenInfo)
         const sourceSelector = '#editor'
         await page.waitForSelector(sourceSelector, { timeout: 10000 })
         const sourceCode = await page.evaluate(
           'var editor = ace.edit("editor"); editor.getValue();'
         )
 
-        const solPath = path.resolve(__dirname, 'sol', symbol + '.sol')
+        const solPath = path.resolve(__dirname, 'sol', tokenInfo + '.sol')
         await fs.writeFile(solPath, sourceCode, 'utf-8', err => {
           if (err) {
             console.error(err)
